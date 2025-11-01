c0fd2605bacbeebbd338f9403602de968be6f786
Klarkasaurus <klarkus@hotmail.com>
2025-11-01 10:50:42 +0000

    Event Viewer script

--- EventLog24hrs.ps1
+++ EventLog24hrs.ps1
@@ -2,4 +2,4 @@
     LogName = 'System', "Security", 'Application'
     Level = 1,2,3  # 1=Critical, 2=Error, 3=Warning
     StartTime = (Get-Date).AddDays(-1)
-} | Format-Table -wrap TimeCreated, LevelDisplayName, Id, Message -AutoSize | Export-Csv "C:\Users\admin\Documents\Scripts\PS\EventLog24hrs.txt"
+} | Format-Table -wrap TimeCreated, LevelDisplayName, Id, Message -AutoSize | Out-File "C:\Users\admin\Documents\Scripts\PS\EventLog24hrs.txt"
