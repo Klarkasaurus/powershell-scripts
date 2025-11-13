c9f2e00d1d686ebb9474c1a3da5c699a792d43d0
Klarkasaurus <klarkus@hotmail.com>
2025-11-13 18:29:21 +0000

    Full Backup Script

---
+++ BackupDocs.ps1
@@ -0,0 +1,6 @@
+﻿# BackupDocs.ps1
+$source = "$env:USERPROFILE\OneDrive\Documents"
+$destination = "D:\Klark\Documents_$(Get-Date -Format yyyy-MM-dd)"
+New-Item -ItemType Directory -Force -Path $destination | Out-Null
+Copy-Item -Path $source\* -Destination $destination -Recurse -Force
+Write-Output "Backup completed at $(Get-Date)" | Out-File "C:\Users\klark\OneDrive\Documents\PS Scripts\BackupLog.txt" -Append
